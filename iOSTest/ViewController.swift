0fd4db83230b00d4da83be21a90c3450c24773f5
Антонов Андрей <an_42@mail.ru>
2021-05-04 12:53:30 +0700

    Initial Commit

---
+++ iOSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by Андрей Антонов on 04/05/2021.
+//  Copyright © 2021 Андрей Антонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
